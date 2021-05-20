5ec590e8172e24f0c3819127d6c43dca7c83495d
walkonmars36 <mark@walkonmars.co.uk>
2021-05-20 22:48:52 +0100

    completed 2nd challenge

--- 03-Javascript/challenges/JS-challenge-1/script.js
+++ 03-Javascript/challenges/JS-challenge-1/script.js
@@ -8,3 +8,15 @@ function calculateYearsUntilRetirement(event) {
     alert(`You have ${yearsLeft} years before you can retire`);
   }
 }
+
+function getGreetingInFrench(event) {
+  const greeting = event.target.value;
+
+  if (greeting == "Hello") {
+    alert("Bonjour");
+  } else if (greeting == "Goodbye") {
+    alert("Au revoir");
+  } else {
+    alert("I'm sorry I don't understand that!");
+  }
+}
