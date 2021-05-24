3f60449bdbc0672a33ddeba07ad2662e22bbe2ce
walkonmars36 <mark@walkonmars.co.uk>
2021-05-24 13:57:29 +0100

    started sode along

---
+++ 03-Javascript/grade-calculator/script.js
@@ -0,0 +1,28 @@
+function handleCalculateGrade(event) {
+  const grade = event.target.value;
+
+  let percentage;
+  switch (grade) {
+    case "A":
+      percentage = "90-100%";
+      break;
+
+    case "B":
+      percentage = "75 - 90%";
+      break;
+
+    case "C":
+      percentage = "50 - 75%";
+      break;
+
+    case "D":
+      percentage = "25 - 50%";
+      break;
+
+    case "E":
+      percentage = "0 - 25%";
+      break;
+  }
+
+  alert("The percentage was " + percentage);
+}
