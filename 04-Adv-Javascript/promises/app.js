5b03934573bbdc64e63bee291328db77d12e394f
Mark Lawson <mark@walkonmars.co.uk>
2021-10-01 17:07:57 +0100

    completed JS

--- 04-Adv-Javascript/promises/app.js
+++ 04-Adv-Javascript/promises/app.js
@@ -1,5 +1,3 @@
-console.log("hello");
-
 // What is ap promise?
 // A promise has 3 states - pending, resolved and rejected
 
@@ -42,24 +40,31 @@ console.log("hello");
 // - the first food that it pairs well with (p)
 
 const getBeer = () => {
+  const generateBeerBtn = document.querySelector(".beer-button");
+  const h1BeerName = document.querySelector(".beer-name");
+  const h4Tagline = document.querySelector(".tagline");
+  const descriptionText = document.querySelector(".description");
+  const alcByVolume = document.querySelector(".abv");
+  const firstFood = document.querySelector(".food-pairing");
+
   fetch("https://api.punkapi.com/v2/beers/random")
     .then((res) => res.json())
 
     .then((beerObject) => {
-      console.log(beerObject);
-
       const beerName = beerObject[0].name;
       const tagline = beerObject[0].tagline;
       const description = beerObject[0].description;
       const abv = beerObject[0].abv;
       const foodPairing = beerObject[0].food_pairing[0];
 
-      console.log(beerName);
-      console.log(tagline);
-      console.log(description);
-      console.log(abv);
-      console.log(foodPairing);
+      h1BeerName.innerHTML = beerName;
+      h4Tagline.innerHTML = tagline;
+      descriptionText.innerHTML = description;
+      alcByVolume.innerHTML = abv;
+      firstFood.innerHTML = foodPairing;
     });
+
+  generateBeerBtn.addEventListener("click", getBeer);
 };
 
 getBeer();
