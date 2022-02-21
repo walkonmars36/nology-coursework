bbbe1d3515902b5bdcffb31b8d908468a9a936a2
Mark Lawson <mark@walkonmars.co.uk>
2022-02-21 14:33:52 +0000

    initial commit

---
+++ 05-React/ticket-tracker/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const App = () => {
+  return <p>Hello App</p>;
+};
+
+export default App;
+
+// 1. Change title in public/index.hmtl
+// 2. Rename src/App.js -> App.jsx
+// 3. Clear out code from return in App.jsx
+// 4. Clear out code from App.css
+// 5. Add global reset to index.css
+// 6. Install sass
+// 7. Rename App.css -> App.module.scss and change import
